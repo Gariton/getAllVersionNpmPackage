c4f9ed1b1e55d6795c3d9acf82b437159e74b92f
root <root@local.gariton.jp>
2022-09-13 00:57:46 +0900

    依存パッケージをすべて持ってくるように変更

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const { execSync } = require('child_process');
-const { getPackageInfo } = require('./utils')
+const { npmPack } = require('./utils')
 const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
 
 (async function () {
 
@@ -15,7 +12,6 @@ const readline = require('readline');
 				if (err) { throw err; }
 			});
 		}
-		let historyJson = JSON.parse(fs.readFileSync('./history.json', 'utf8'));
 
 		// outputsフォルダがなければ作成
 		if (!fs.existsSync('./outputs')) {
@@ -31,48 +27,9 @@ const readline = require('readline');
 			return;
 		} else {
 			// 指定のパッケージ名が存在するかチェック
-			for (let name of packageNames) {
-				let versions = await getPackageInfo(name);
-				if (versions === false) {
-					process.stdout.write(`パッケージ名:${name}は存在しません\n`);
-				} else {
-					
-					process.stdout.write(`パッケージ名:${name}に${versions.length}個のバージョンが見つかりました\n`);
-
-					// バージョンごと処理
-					for (let version of versions) {
-						process.stdout.write(`\tversion ${version}:処理中`);
-						// history.jsonになければ追加&処理
-						let historyInfo = historyJson[name];
-						if (historyInfo == undefined) {
-							// パッケージ名が過去にDLした履歴がない
-							execSync(`cd outputs && npm pack ${name}@${version}`);
-							//履歴に追加
-							historyJson[name] = {
-								versions: [version]
-							}
-							process.stdout.write(`\n`);
-						} else {
-							// パッケージ名では過去にある
-							// バージョンは?
-							if (historyInfo.versions.includes(version)) {
-								// ある
-								readline.cursorTo(process.stdout, 0);
-								process.stdout.write(`\tversion ${version}:DL済み\n`);
-							} else {
-								// ない
-								execSync(`cd outputs && npm pack ${name}@${version}`);
-								historyInfo.versions.push(version);
-								process.stdout.write(`\n`);
-							}
-						}
-						
-					}
-				}
+			for (let packageName of packageNames) {
+				await npmPack(packageName);
 			}
-			fs.writeFileSync('./history.json', JSON.stringify(historyJson), (err) => {
-				if (err) throw err;
-			});
 		}
 
 	} catch (error) {
--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
 const packageInfo = require('package-info');
 const packageVersions = require('npm-package-versions');
+const { execSync } = require('child_process');
+const fs = require('fs').promises;
 
 const getPackageInfo = async (packageName) => {
     try {
@@ -23,6 +25,60 @@ function packageVersionsSync (packageName) {
     });
 }
 
+async function npmPack (package) {
+    let versions = await getPackageInfo(package);
+    if (!versions) {
+        console.log(`${package}というパッケージが見つかりませんでした。`);
+        return false;
+    }
+
+    // バージョンごと処理
+    for (let version of versions) {
+        try {
+            let historyJson = JSON.parse(await fs.readFile('./history.json', 'utf8'));
+
+            let historyInfo = historyJson[package];
+            if (historyInfo == undefined) {
+                // パッケージ名が過去にDLした履歴がない
+                console.log(`${package}@${version}:処理中`);
+                execSync(`cd outputs && npm pack ${package}@${version}`);
+                //履歴に追加
+                historyJson[package] = {
+                    versions: [version]
+                }
+                await fs.writeFile('./history.json', JSON.stringify(historyJson));
+                let deps = execSync(`npm info ${package} dependencies --json`);
+                deps = JSON.parse(deps.toString());
+                let depPackageNames = Object.keys(deps);
+                
+                for (let dep of depPackageNames) {
+                    await npmPack(dep);
+                }
+            } else {
+                // パッケージ名では過去にある
+                // バージョンは?
+                if (!historyInfo.versions.includes(version)) {
+                    // ない
+                    execSync(`cd outputs && npm pack ${package}@${version}`);
+                    historyInfo.versions.push(version);
+                    await fs.writeFile('./history.json', JSON.stringify(historyJson));
+                    let deps = execSync(`npm info ${package} dependencies --json`);
+                    deps = JSON.parse(deps.toString());
+                    let depPackageNames = Object.keys(deps);
+                    
+                    for (let dep of depPackageNames) {
+                        await npmPack(dep);
+                    }
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+}
+
 module.exports = {
-    getPackageInfo
+    getPackageInfo,
+    npmPack
 }
